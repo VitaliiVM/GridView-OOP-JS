6ae4b970b4b7bf964f1319a57b7555f252016517
VitaliiVM <69699528+VitaliiVM@users.noreply.github.com>
2021-05-03 23:41:19 +0300

    add params

---
+++ GridView.js
@@ -0,0 +1,14 @@
+
+
+class GridView {
+    /**
+     *  properties
+     *  @param [array] _tableClass - css классы оформления
+     *  @param [array] data  - выходные данные
+     *  @param [array] attribute - управляем что выводим
+     *  @param [array] _element  - куда выводить таблицу
+     *  @param [array] _header  - заголовок таблицы
+     *  @param [array] _headerClass  - css классы заголовка
+     */
+
+}

1d22d34b043879c8efeb123c27a5b895807bee67
VitaliiVM <69699528+VitaliiVM@users.noreply.github.com>
2021-05-03 23:51:28 +0300

    set header

--- GridView.js
+++ GridView.js
@@ -5,10 +5,34 @@ class GridView {
      *  properties
      *  @param [array] _tableClass - css классы оформления
      *  @param [array] data  - выходные данные
-     *  @param [array] attribute - управляем что выводим
+     *  @param [array] _attribute - управляем что выводим
      *  @param [array] _element  - куда выводить таблицу
      *  @param [array] _header  - заголовок таблицы
      *  @param [array] _headerClass  - css классы заголовка
      */
+    constructor() {
+        this._header = "";
+        this._headerClass = [];
+        this._tableClass = [];
+        this._element = 'body';
+        this._attribute = [];
+    }
 
+    /**
+     * Method set header
+     */
+    set header(header) {
+        if (typeof header === 'string' && header.trim() != '') {
+            this._header = header.trim();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Method for show GridWiewTable
+     */
+    render() {
+
+    }
 }
